55563c4e580e15b0affc74666a163ad76f679d8a
Daveobaro <daveobaro8@gmail.com>
2024-08-29 10:03:59 +0100

    adding-more-ground-checks
    Setting up project

---
+++ travel-journal/src/data.js
@@ -0,0 +1,32 @@
+[
+	{
+		id: '1',
+		country: 'Croatia',
+		coverImage: 'ewewew',
+		locationName: 'Hvar',
+		dateTravelled: '12 Jan, 2021 - 24 Jan, 2021',
+		description:
+			'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+		mapLink: 'ewwewew',
+	},
+	{
+		id: '2',
+		country: 'Croatia',
+		coverImage: 'ewewew',
+		locationName: 'Hvar',
+		dateTravelled: '12 Jan, 2021 - 24 Jan, 2021',
+		description:
+			'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+		mapLink: 'ewwewew',
+	},
+	{
+		id: '3',
+		country: 'Croatia',
+		coverImage: 'ewewew',
+		locationName: 'Hvar',
+		dateTravelled: '12 Jan, 2021 - 24 Jan, 2021',
+		description:
+			'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+		mapLink: 'ewwewew',
+	},
+];
---
+++ travel-journal/src/journal-navbar.jsx
@@ -0,0 +1,14 @@
+function JournalNavbar() {
+	return (
+		<header>
+			<a>
+				<p>
+					<i class='fa-solid fa-earth-americas fa-spin fa-spin-reverse'></i> my
+					travel journal.
+				</p>
+			</a>
+		</header>
+	);
+}
+
+export default JournalNavbar;
